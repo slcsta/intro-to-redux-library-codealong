6bcfa23ad6e1d66703035cbac8737c91476129e4
Sarah <sarahlchavez@gmail.com>
2021-11-27 05:40:17 -0800

    commit tree a5dad27efc55a35ea8cf6d48ec6faafce0f8a629

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore } from "redux";
 import counterReducer from './reducers/counterReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counterReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// Now with the above setup, let's pass store down to App as a prop so it can access the Redux store
+// Here we pass our newly created store to our App component as a prop.
+ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
+);
